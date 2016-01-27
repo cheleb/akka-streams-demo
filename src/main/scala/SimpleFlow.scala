bce77d7d2171fcd8dc71df2f871748bab5b8c9c9
olivier.nouguier <olivier.nouguier@gmail.com>
2016-01-27 23:10:40 +0100

    Add simple graph

--- src/main/scala/SimpleFlow.scala
+++ src/main/scala/SimpleFlow.scala
@@ -1,8 +1,31 @@
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Success
+
 /**
   * Created by chelebithil on 27/01/2016.
   */
 object SimpleFlow extends App {
 
-  print("Helo ii")
+  implicit val actorSystem = ActorSystem("reactiveStreams")
+  implicit val materilizer = ActorMaterializer()
+
+
+  val source = Source(1 to 10)
+  val sink = Sink.fold[Int, Int](0)(_ + _)
+
+  val fut: Future[Int] = source.runWith(sink)
+  fut.onSuccess {
+    case i =>
+      println(i)
+  }
+  fut.onComplete {
+    case Success(e) =>
+      actorSystem.terminate()
+  }
 
 }
---
+++ src/main/scala/SimpleGraph.scala
@@ -0,0 +1,34 @@
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, ClosedShape}
+import akka.stream.scaladsl._
+
+/**
+  * Created by chelebithil on 27/01/2016.
+  */
+object SimpleGraph extends App {
+
+  implicit val actorSystem = ActorSystem("reactiveStreams")
+  implicit val materilizer = ActorMaterializer()
+
+
+  val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[Unit] =>
+    import GraphDSL.Implicits._
+
+    val in = Source(1 to 10)
+    val out = Sink.foreach[Int](x => println(x))
+
+    val bcast = builder.add(Broadcast[Int](2))
+    val merge = builder.add(Merge[Int](2))
+
+    val f1, f2, f3, f4 = Flow[Int].map(_ + 10)
+
+    in ~> f1 ~> bcast ~> f2 ~> merge ~> f3 ~> out
+    bcast ~> f4 ~> merge
+    ClosedShape
+
+
+  })
+
+  val f = g.run()
+
+}
